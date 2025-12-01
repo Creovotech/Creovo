01edc6879b3257bbda74f5851390f4a2846e33ad
Sayan2908 <sayandeysarkar2003@gmail.com>
2025-12-01 15:31:27 +0530

    feat: layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import { ViewTransitions } from "next-view-transitions";
 import { AppNavbar } from "@/components/layout/navbar";
 import { cn } from "@/lib/utils";
@@ -34,7 +34,7 @@ export default function RootLayout({
           )}
         >
           <ViewTransitions >
-            <AppNavbar />
+            {/* <AppNavbar /> */}
             {children}
             <Footer />
           </ViewTransitions>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,12 @@ import graph_card from '@/components/cards/graph_card.json';
 import globe_card from '@/components/cards/globe_card.json';
 import social_media_card from '@/components/cards/social_media_card.json';
 import ScrollToTop from '@/components/scroll-to-top';
+import { AppNavbar } from '@/components/layout/navbar';
 
 export default function LandingPage() {
   return (
     <div className="relative overflow-hidden w-full">
+      <AppNavbar />
       <AmbientColor />
       <Hero />
       <Features heading={'Engineering for Global Scale'} sub_heading={'From local startups to global ecosystems, we architect solutions designed to handle millions of interactions without breaking a sweat'} globe_card={globe_card} ray_card={ray_card} graph_card={graph_card} social_media_card={social_media_card} />
